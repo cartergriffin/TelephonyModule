d772414aa541348f72f4e66047c424eab9f2d70f
Carter Griffin <Carter.R.Griffin@gmail.com>
2025-05-12 15:39:38 -0700

    add Get-ObjectReplicationTime

---
+++ public/Get-ObjectReplicationTime.ps1
@@ -0,0 +1,28 @@
+function Get-ObjectReplicationTime {
+    [Cmdletbinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$DistinguishedName
+    )
+    $dcObjects = Get-ADDomainController -Filter *
+    $out = [System.Collections.Generic.List[object]]::new()
+    # parallelize later for perf
+    foreach ($dc in $dcObjects) {
+        $current = $null
+        $queryParams = @{
+            Properties  = 'whenChanged'
+            Server      = $dc.HostName
+            ErrorAction = 'SilentlyContinue'
+            Filter      = "DistinguishedName -eq '$($DistinguishedName)'"
+        }
+        $current = Get-ADObject @queryParams
+        $out.Add([PScustomObject]@{
+            value = $current.Name
+            mod = $current.whenChanged
+            server = $dc.Name
+			site = $dc.site
+            })
+    }
+    $output = $out | Sort-Object -Property mod -Descending
+    Write-Output $output
+}
