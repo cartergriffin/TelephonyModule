7279aa539df34708719b297dfce2dcaa0659ea36
Carter Griffin <Carter.R.Griffin@gmail.com>
2025-06-08 09:03:56 -0700

    add parse-ini plans

--- Parse-SBCIni.ps1
+++ Parse-SBCIni.ps1
@@ -1,5 +1,138 @@
-# hello world
+# adding new function to improve atatrunkgroup logic
+# need to modularize, make functions for trunkgroup, dial plan
+# ip2ipinbound and outbound manipulation, parse each one just like this one 
+<# if ($currentSectionName = 'TrunkGroup') {
+    if ($line -match "^TrunkGroup\s+(\d{1,3})\s+=\s*(.+)\;") {
+        $attrList = $matches[2].Split(',').Trim()
+        $trunkGroupIndex = $matches[1]
+        $phoneNumExtracted = $attrList[4].Trim('"')
 
-$IniLines = Get-AudiocodesIniAsObjects
+        $stdOut = [PSCustomObject]@{
+            DeviceIP = $deviceIP
+            TrunkGroupIndex = $trunkGroupIndex
+            telephoneNumber = $phoneNumExtracted
+            TrunkGroupId = $attrList[0]
+            BChannelPort = $attrlist[2]
+            FXSModule = $attrList[7]
+            FullLine = $line
+        } #>
+function Get-AudiocodesATATrunkGroupAssignments {
+    param(
+        [string]$phoneFilter
+    )
+    # using function defined in ~\public\Get-AudioCodesIniAsObjects.ps1. Gets ini from
+    # devices and returns one object per ini line containing deviceIP + full line
+    $allIniLines = Get-AudioCodesIniAsObjects
 
-$sbcLines = $iniLines | Where-Object {$_.DeviceIP -eq "10.15.75.103"}
+    $groupedLinesByDevice = $allIniLines | Group-object -Property DeviceIP
+
+    $iniByDevice = @{}
+
+    foreach ($deviceGroup in $groupedLinesByDevice) {
+        $deviceIP = $deviceGroup.Name
+        $lines = $deviceGroup.Group | ForEach-Object { $_.Line }  # Strip the DeviceIP now
+
+        $iniByDevice[$deviceIP] = $lines
+    }
+
+    # do initial cheap regex match on all lines to find format sections
+    function is_section_format {
+        param ( [string]$iniLine )
+        if ($iniLine -match "^FORMAT") {
+            return $true
+        } else {
+            return $false
+        }
+    }
+    # unused
+    function is_section_header {
+        param ( [string]$iniLine )
+            if ($iniLine -match "^\[(.+?)\]") {
+                $currentSectionName = $matches[1]
+                continue
+            }
+    }
+    # unused
+    function parse_section_format {
+        param ( [string]$iniLine )
+        if ($iniLine -match "^FORMAT\s+(.+?)\s*=\s*(.+)\;") {
+            $indexName = $matches[1]
+            $indexAttributes = $matches[2].Split(',').Trim()
+        }
+    }
+
+    # dotnet list object for performance
+    $output = [System.Collections.Generic.List[object]]::new()
+
+    # init hashtable for dictionary
+    $sectionSchemasByDevice = @{}
+
+    # forget dictionary for functional reasons just get trunkgroup port assignments
+    $trunkGroupOutput = [System.Collections.Generic.List[object]]::new()
+
+    foreach ($deviceIP in $iniByDevice.Keys) {
+        $lines = $iniByDevice[$deviceIP]
+
+        # init empty per-device hashtable for all section schemas
+        $sectionSchemasByDevice[$deviceIP] = @{}
+
+        foreach ($line in $lines) {
+            if ($line -match "^\[\s(.+?)\s\]") {
+
+                # track current section name for OS normalization + parsing logic
+                $currentSectionName = $matches[1]
+                $output.Add($matches[1])
+                continue
+            }
+            if (is_section_format -iniLine $line) {
+                if ($line -match "^FORMAT\s+(.+?)\s*=\s*(.+)\;") {
+                    # dynamically return index attrs for each diff section
+                    $indexAttributes = $matches[2].Split(',').Trim()
+                    
+                    # normalize across different audiocodes os versions
+                    if ($matches[1] -eq 'Index') {
+                        $indexName = "$currentSectionName`_Index"
+                    } else {
+                        $indexName = $matches[1]
+                        # unused
+                        $verboseIndexAttrs = $indexAttributes
+                    }
+                    
+                    # add to dictionary
+                    $sectionSchemasByDevice[$deviceIP][$indexName] = $indexAttributes
+                }
+            }
+            # forget abt dictionary and use existing logic to get all trunkgroup
+            # assignments as nice objects
+            if ($currentSectionName = 'TrunkGroup') {
+                if ($line -match "^TrunkGroup\s+(\d{1,3})\s+=\s*(.+)\;") {
+                    $attrList = $matches[2].Split(',').Trim()
+                    $trunkGroupIndex = $matches[1]
+                    $phoneNumExtracted = $attrList[4].Trim('"')
+
+                    $stdOut = [PSCustomObject]@{
+                        DeviceIP = $deviceIP
+                        TrunkGroupIndex = $trunkGroupIndex
+                        telephoneNumber = $phoneNumExtracted
+                        TrunkGroupId = $attrList[0]
+                        BChannelPort = $attrlist[2]
+                        FXSModule = $attrList[7]
+                        FullLine = $line
+                    }
+
+                    # if filter passed, filter
+                    if ($phoneFilter) {
+                        if ($phoneNumExtracted -match "\$phoneFilter\d{4}") {
+                            $trunkGroupOutput.Add($stdOut)
+                        }
+                    # if not, return all trunk group rows as output
+                    } else {
+                        $trunkGroupOutput.Add($stdOut)
+                    }
+                }
+            }
+        }
+
+    }
+    return $trunkGroupOutput
+}
